a48869113d3a2a95400f61815175b3ee95fa6545
Surojit <surojitsantra97@gmail.com>
2022-05-17 12:38:06 +0530

    Added test cases

--- src/main/java/string/calculator/Utility.java
+++ src/main/java/string/calculator/Utility.java
@@ -5,7 +5,49 @@ public class Utility {
 	public static boolean isValidStringForAddition(String numbers) {
 		if (numbers == null || numbers.trim().equals(""))
 			return false;
+
+		numbers = numbers.trim();
+
+		char[] numbersChar = numbers.toCharArray();
+
+		if (!isNumber(numbersChar[0]) && (numbersChar[0] != '-'))
+			return false;
+		if (!isNumber(numbersChar[numbersChar.length - 1]))
+			return false;
+
+//		boolean lastCharComma = false;
+//		boolean lastCharNewLine = false;
+
+		for (int i = 0; i < numbersChar.length - 1; i++) {
+			if (!isValidChar(numbersChar[i]))
+				return false;
+
+			if (numbersChar[i] == '-' && !isNumber(numbersChar[i + 1]))
+				return false;
+			if ((numbersChar[i] == ',' || numbersChar[i] == '\n')
+					&& (numbersChar[i + 1] == ',' || numbersChar[i + 1] == '\n'))
+				return false;
+
+		}
+
 		return true;
+	}
+
+	private static boolean isValidChar(char num) {
+		if (isNumber(num))
+			return true;
+		if (num == ',')
+			return true;
+		if (num == '\n')
+			return true;
+		if (num == '-')
+			return true;
+		return false;
+
+	}
+
+	private static boolean isNumber(char number) {
+		return number >= '0' && number <= '9';
 	}
 
 }
--- src/test/java/string/calculator/UtilityTest.java
+++ src/test/java/string/calculator/UtilityTest.java
@@ -7,10 +7,40 @@ public class UtilityTest {
 	@Test
 	public void shouldAnswerWithTrue() {
 //		Invalid cases
-		String numbers1 = "";
-		String numbers2 = "    ";
+		String invalidNumbers1 = "";
+		String invalidNumbers2 = "    ";
+		String invalidNumbers3 = "  12,,2  ";
+		String invalidNumbers4 = "12\n,2";
+		String invalidNumbers5 = ",12\n2";
+		String invalidNumbers6 = ",12\na2";
+		String invalidNumbers7 = ",1a2\n2";
+		String invalidNumbers8 = ",12\nx2";
+		String invalidNumbers9 = ",12,2,3a";
+		String invalidNumbers10 = ",12cv2";
+
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers1));
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers2));
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers3));
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers4));
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers5));
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers6));
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers7));
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers8));
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers9));
+		Assert.assertFalse(Utility.isValidStringForAddition(invalidNumbers10));
+
+		// Valid Case
+		String validNumbers1 = "12\n2";
+		String validNumbers2 = "12,2";
+		String validNumbers3 = "-12\n2";
+		String validNumbers4 = "12\n-2";
+		String validNumbers5 = "-12,2";
 		
-		Assert.assertFalse(Utility.isValidStringForAddition(numbers1));
-		Assert.assertFalse(Utility.isValidStringForAddition(numbers2));
+		Assert.assertTrue(Utility.isValidStringForAddition(validNumbers1));
+		Assert.assertTrue(Utility.isValidStringForAddition(validNumbers2));
+		Assert.assertTrue(Utility.isValidStringForAddition(validNumbers3));
+		Assert.assertTrue(Utility.isValidStringForAddition(validNumbers4));
+		Assert.assertTrue(Utility.isValidStringForAddition(validNumbers5));
+
 	}
 }
